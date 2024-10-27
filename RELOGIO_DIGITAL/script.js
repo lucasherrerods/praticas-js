325059ef91d1782736a8739dbdfc0d65e3264cad
Lucas Herrero <lucasherrerosilva.lh@gmail.com>
2024-10-26 22:38:57 -0300

    hora atual

---
+++ RELOGIO_DIGITAL/script.js
@@ -0,0 +1,24 @@
+const hours = document.querySelector('#hours')
+const minutes = document.querySelector('#minutes')
+const seconds = document.querySelector('#seconds')
+
+const clock = setInterval(function time() {
+    let dateToday = new Date()
+    let hr = dateToday.getHours()
+    let min = dateToday.getMinutes()
+    let s = dateToday.getSeconds()
+
+    if (hr < 10) {
+        hr = `0${hr}`
+    }
+    if (min < 10) {
+        min = `0${min}`
+    }
+    if (s < 10) {
+        s = `0${s}`
+    }
+
+    hours.textContent = hr
+    minutes.textContent = min
+    seconds.textContent = s
+})
