b71365e35ef5437cfbdf09afcd70aedef41c2d90
Lucas Herrero <lucasherrerosilva.lh@gmail.com>
2024-11-27 00:09:18 -0300

    completando tasks

--- todo_list/assets/js/script.js
+++ todo_list/assets/js/script.js
@@ -5,7 +5,10 @@ const completeList = document.querySelector('.list-task')
 let listTask = []
 
 function addNewTask() {
-    listTask.push(inputTask.value)
+    listTask.push({
+        task: inputTask.value,
+        completed: false
+    })
 
     inputTask.value = ''
 
@@ -18,12 +21,12 @@ function showTasks() {
 
     let newLi = ''
 
-    listTask.forEach((task, index) => {
+    listTask.forEach((item, index) => {
 
         newLi = newLi + `
-        <li class="task">
-            <img src="assets/img/checked.png" alt="checked">
-            <p>${task}</p>
+        <li class="task ${item.completed && "done"}">
+            <img src="assets/img/checked.png" alt="checked" onclick="completeTask(${index})">
+            <p>${item.task}</p>
             <img src="assets/img/trash.png" alt="lixeira" onclick="removeTask(${index})">
         </li>
         `
@@ -34,6 +37,12 @@ function showTasks() {
 
 function removeTask(index) {
     listTask.splice(index, 1)
+
+    showTasks()
+}
+
+function completeTask(index) {
+    listTask[index].completed = !listTask[index].completed
 
     showTasks()
 }
