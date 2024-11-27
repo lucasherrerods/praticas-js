e1b2b842d2cabf671d0fbcfaf42921a1279924f5
Lucas Herrero <lucasherrerosilva.lh@gmail.com>
2024-11-26 23:53:23 -0300

    deletando task

--- todo_list/assets/js/script.js
+++ todo_list/assets/js/script.js
@@ -18,16 +18,22 @@ function showTasks() {
 
     let newLi = ''
 
-    listTask.forEach(task => {
+    listTask.forEach((task, index) => {
 
         newLi = newLi + `
         <li class="task">
             <img src="assets/img/checked.png" alt="checked">
             <p>${task}</p>
-            <img src="assets/img/trash.png" alt="lixeira">
+            <img src="assets/img/trash.png" alt="lixeira" onclick="removeTask(${index})">
         </li>
         `
     })
 
     completeList.innerHTML = newLi
+}
+
+function removeTask(index) {
+    listTask.splice(index, 1)
+
+    showTasks()
 }
